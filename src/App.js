054c128ac3830ac887af488b6f26b7823c4d027d
Raggio11 <Raggio11@users.noreply.github.com>
2021-02-23 16:19:09 -0700

    fix number change issues
    When changing how many  number of events wanted displayed it would not show anything until you search for either a city or click on all.

--- src/App.js
+++ src/App.js
@@ -1,79 +1,62 @@
 import React, { Component } from 'react';
 import './App.css';
-import "./nprogress.css"
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from "./api";
-
-
+import { getEvents, extractLocations } from './api';
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
-    numberOfEvents: 32
-  }
+    eventCount: 5,
+  };
+
+  updateEvents = (location, eventCount) => {
+    let locationEvents;
+    getEvents().then((events) => {
+      if (location === 'all' && eventCount === 0) {
+        locationEvents = events;
+      } else if (location !== 'all' && eventCount === 0) {
+        locationEvents = events.filter((event) => event.location === location);
+        console.log(eventCount);
+      } else if (location === '' && eventCount > 0) {
+        locationEvents = events.slice(0, eventCount);
+      }
+      this.setState({
+        events: locationEvents,
+      });
+    });
+  };
 
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
-      const { numberOfEvents } = this.state;
       if (this.mounted) {
-        const filteredEvents = events.slice(0, numberOfEvents);
-        this.setState({ 
-          events: filteredEvents,
-          locations: extractLocations(events),
-        });
-      }
-
-      if (!navigator.onLine) {
         this.setState({
-          infoText: 'You are currently offline.  The list of events may not be up-to-date.'
+          events: events.slice(0, this.state.eventCount),
+          locations: extractLocations(events),
         });
       }
     });
   }
-  componentWillUnmount(){
+
+  componentWillUnmount() {
     this.mounted = false;
   }
 
-  updateEvents = (location, eventCount) => {
-    const { currentLocation, numberOfEvents } = this.state;
-    if (location) {
-      getEvents().then((events) => {
-        const locationEvents =
-          location === 'all'
-          ? events
-          : events.filter((event) => event.location === location);
-        const filteredEvents = locationEvents.slice(0, numberOfEvents);
-        this.setState({
-          events: filteredEvents,
-          currentLocation: location,
-        });
-      });
-    } else {
-      getEvents().then((events) => {
-        const locationEvents =
-          currentLocation === 'all'
-          ? events
-          : events.filter((event) => event.location === currentLocation);
-        const filteredEvents = locationEvents.slice(0, eventCount);
-        this.setState({
-          events: filteredEvents,
-          numberOfEvents: eventCount,
-        });
-      });
-    }
-  };
-
-
-
   render() {
     return (
-      <div className="App">
-        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
-        <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents}/>
+      <div className='App'>
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+          eventCount={this.state.eventCount}
+        />
+        <NumberOfEvents
+          eventCount={this.state.eventCount}
+          updateEvents={this.updateEvents}
+        />
         <EventList events={this.state.events} />
       </div>
     );
